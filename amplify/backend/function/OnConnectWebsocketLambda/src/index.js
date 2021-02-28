0b3af2cad88e993edd52d87b7ef2487deda86d8b
Malcolm Childs <malcolm@yourcheck.co>
2021-02-28 13:50:57 -0500

    Resolves errors and gets webSocket API back to working state

--- amplify/backend/function/OnConnectWebsocketLambda/src/index.js
+++ amplify/backend/function/OnConnectWebsocketLambda/src/index.js
@@ -1,15 +1,22 @@
-exports.handler = async ({ requestContext }) => {
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_WEBSOCKETCONNECTIONIDTABLE_ARN
+	STORAGE_WEBSOCKETCONNECTIONIDTABLE_NAME
+Amplify Params - DO NOT EDIT */exports.handler = async ({ requestContext }, context) => {
     const connectionId = requestContext.connectionId;
     try {
-        console.log(`Websocket connected successfully! ConnectionId: ${connectionId}`)
+        console.log(`Websocket connected successfully! connectionId: ${connectionId}`)
         return {
             statusCode: 200,
             body: JSON.stringify({ connectionId })
         }
     } catch (error) {
+        const errorMessage = `Error connecting to the websocket. ${error}`
+        console.error(errorMessage)
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: `Error connecting to the websocket. ${error}` })
+            body: JSON.stringify({ message: errorMessage })
         }
     }
 };
